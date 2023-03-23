67426dd47f8c35fc4c194885aaa69caf0bf3d9cf
BautiLentz <bautistalentz@gmail.com>
2023-03-23 08:05:51 -0300

    Avances

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express= require('express') 
 const app= express()
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+const publicPath = path.resolve('public');
+app.use(express.static(publicPath));
+app.use(express.urlencoded( { extended : false}))
+
 
 app.listen(3000,()=>console.log('Esto fue exsitoso'))
 
